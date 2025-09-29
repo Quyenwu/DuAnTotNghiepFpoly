c9e142f58e5d5989f80a8f156e5def3b591bb481
phuong-ba <phuongtvph52606@gmail.com>
2025-09-29 10:44:49 +0700

    update phuong

---
+++ src/main/java/com/example/webaothu/WebAoThuApplication.java
@@ -0,0 +1,13 @@
+package com.example.webaothu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAoThuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAoThuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webaothu/WebAoThuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webaothu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAoThuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
