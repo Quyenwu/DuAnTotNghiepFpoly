a052a52eb50ebc857938ccb26b7a066d8b4cf93b
PhanTac1122001 <tintert1703@gmail.com>
2025-10-07 11:12:52 +0700

    quan ly nhan vien

---
+++ DATN/BE/The_Autumn/src/main/java/com/example/the_autumn/controller/ChucVuController.java
@@ -0,0 +1,27 @@
+package com.example.the_autumn.controller;
+
+import com.example.the_autumn.model.response.ResponseObject;
+import com.example.the_autumn.service.ChucVuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chuc-vu")
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"})
+public class ChucVuController {
+
+    @Autowired
+    private ChucVuService chucVuService;
+
+    @GetMapping
+    public ResponseObject<?> getAllChucVu(){
+        return new ResponseObject<>(chucVuService.getAllChucVu());
+    }
+    
+
+
+
+}
---
+++ DATN/BE/The_Autumn/src/main/java/com/example/the_autumn/model/request/NhanVienRequest.java
@@ -0,0 +1,49 @@
+package com.example.the_autumn.model.request;
+
+import com.example.the_autumn.entity.ChucVu;
+import com.example.the_autumn.entity.HoaDon;
+import com.example.the_autumn.entity.LichSuHoaDon;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class NhanVienRequest {
+
+
+    private Integer id;
+    private Integer chucVuId;
+    private String maNhanVien;
+    private String hoTen;
+    private Boolean gioiTinh;
+    private Date ngaySinh;
+    private String email;
+    private String sdt;
+    private String diaChi;
+    private String hinhAnh;
+    private String taiKhoan;
+    private String matKhau;
+    private Date ngayTao;
+    private Date ngaySua;
+    private Boolean trangThai;
+//    private List<HoaDon> hoaDons;
+//    private List<LichSuHoaDon> lichSuHoaDons;
+}
