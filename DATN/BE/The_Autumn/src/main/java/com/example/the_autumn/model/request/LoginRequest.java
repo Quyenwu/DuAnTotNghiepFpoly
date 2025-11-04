55f861acd4ef921a49930d28634a279d16468cf9
Quyenwu <quyennvph26660@gmail.com>
2025-11-05 00:00:51 +0700

    authen

---
+++ DATN/BE/The_Autumn/src/main/java/com/example/the_autumn/model/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.example.the_autumn.model.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    @NotBlank(message = "Email không được để trống")
+    private String email;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    private String password;
+}
